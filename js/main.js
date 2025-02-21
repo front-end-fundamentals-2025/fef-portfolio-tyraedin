adc5d08e3b635bfbf45529b9f121a02d8dbb3437
tyraedin <edty24pr@student.ju.se>
2025-02-21 17:24:53 +0100

    save

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const redButton = document.getElementById("footer-button");
+const bodyElement = document.querySelector("footer");
+redButton.addEventListener("click", function (event) {
+  bodyElement.style.backgroundColor = "#ff7745";
+});
